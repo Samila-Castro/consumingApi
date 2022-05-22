fdc159605cb5b2123cdf8117239985c8e1d70e76
Samila Vieira <samilav1997@gmail.com>
2022-05-22 17:13:14 -0300

    add: component BookItem

---
+++ components/BookItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function BookItem({ book }){
+  return(
+    <View style={styles.content}>
+      <View style={styles.wraper}>
+        <AntDesign name="user" size={20} color="blue" />
+        <Text style={styles.item}>
+          {book.author}
+        </Text>
+      </View>
+      <View style={styles.wraper}>
+        <MaterialIcons name="title" size={20} color="white" />
+        <Text style={styles.item}>
+          {book.title}
+        </Text>
+      </View>
+      <View style={styles.wraper}>
+        <EvilIcons name="link" size={20} color="white" />
+        <Text style={styles.item}>{book.url}</Text>
+      </View>
+    </View> 
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    backgroundColor: '#1f1e25',
+    marginTop: 20,
+    borderRadius: 15,
+    padding: 15,
+  },
+  wraper: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  item: {
+    color: '#fff',
+  },
+});
